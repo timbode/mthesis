8d01f6fc6f59d44eac9fdad374ee19052a1f5b4e
timlappe <lappet@student.ethz.ch>
2014-10-20 16:28:36 +0200

    faster readout

--- read_system_data.py
+++ read_system_data.py
@@ -1,60 +1,10 @@
 from numpy import *
 from math import *
-import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-matplotlib.rcParams['agg.path.chunksize'] = 20000
-
-constants=[]
-particle_data=[]
-lattice_positions=[]
-lattice_velocities=[]
-
-with open('particle_data.txt') as f:
-	for line in f:
-		if line.startswith("#"):
-			strang=line.strip().split()
-			constants.append(strang)
-		else:	
-			string=line.strip().split()
-			Dict={}
-			Dict['b']=int(string[0])
-			Dict['pos']=float(string[1])
-			Dict['index']=int(string[2])
-			Dict['v']=float(string[3])
-			Dict['xdot['+string[2]+']']=float(string[4])
-			particle_data.append(Dict)
-	f.close()
-	
-constants=constants[:-2]
-print constants, "\n ------------------------------------------------"
-
-'''
 #---------------------------------------------------------------------------------
-
-with open('lattice_positions.txt') as f:
-	for line in f:
-		string=line.strip().split()
-		liste=[]
-		for s in string:
-			liste.append(float(s))
-		lattice_positions.append(liste)
-	f.close()
-
 #---------------------------------------------------------------------------------
-
-with open('lattice_velocities.txt') as f:
-	for line in f:
-		string=line.strip().split()
-		liste=[]
-		for s in string:
-			liste.append(float(s))
-		lattice_velocities.append(liste)
-	f.close()
-	
-#---------------------------------------------------------------------------------	
-'''
 
 def AddInterval(bin_width,c,d,liste,weight,weights): # Interval [c,d]
 	for k in xrange(int(floor(c/bin_width)),int(ceil(d/bin_width))):
@@ -64,13 +14,20 @@ def AddInterval(bin_width,c,d,liste,weight,weights): # Interval [c,d]
 
 #---------------------------------------------------------------------------------
 
+constants=[]
+'''
 L=float(constants[4][2])
 N=int(constants[0][2])
 steps=int(constants[5][2])
 resol=int(constants[6][2])
+'''
+N=0
+L=0
+bin_width=0
+
 bins=50
-bin_width=(N+1)*L/bins
-burn_in=steps/5
+
+burn_in=70000
 
 loc_prob1=[]
 weights1=[]
@@ -78,80 +35,82 @@ loc_prob2=[]
 weights2=[]
 loc_prob3=[]
 weights3=[]
-for j in xrange(0,resol):
-	for i in xrange(0,steps - burn_in):
-		b=particle_data[j*steps + burn_in + i]['b']
-		initial=particle_data[j*steps + burn_in + i-1]['pos']
-		final=particle_data[j*steps + burn_in + i]['pos']
-		weight=1/abs(particle_data[j*steps + burn_in + i]['v'])
+with open('particle_data.txt') as f:
+	counter=0
+	initial=0
+	for k, line in enumerate(f):
+		if line.startswith("#"):
+			strang=line.strip().split()
+			constants.append(strang)
+		else:	
+			if k < (counter*int(constants[5][2]) + burn_in + 9):
+				continue
 				
-		if b < 0:
-			if (b % 2) != 0: # ungerade
-				AddInterval(bin_width,0,initial,loc_prob2,weight,weights2)
-				AddInterval(bin_width,0,final,loc_prob2,weight,weights2)
-				for k in xrange(0,abs(b)-1): # ganze Strecken
-					AddInterval(bin_width,0,(N+1)*L,loc_prob2,weight,weights2)
+			if ((k-9) % int(constants[5][2])) == 0:
+				counter+=1
+				continue
+			print k
+				
+			string=line.strip().split()
+			b=int(string[0])
+			final=float(string[1])
+			v=float(string[3])
 			
-			else: # gerade
-				AddInterval(bin_width,0,initial,loc_prob2,weight,weights2)
-				AddInterval(bin_width,final,(N+1)*L,loc_prob2,weight,weights2)
-				for k in xrange(0,abs(b)-1): # ganze Strecken
-					AddInterval(bin_width,0,(N+1)*L,loc_prob2,weight,weights2)
-		
-		
-		if b > 0:
-			if (b % 2) != 0: # ungerade
-				AddInterval(bin_width,initial,(N+1)*L,loc_prob3,weight,weights3)
-				AddInterval(bin_width,final,(N+1)*L,loc_prob3,weight,weights3)
-
-				for k in xrange(0,abs(b)-1): # ganze Strecken
-					AddInterval(bin_width,0,(N+1)*L,loc_prob3,weight,weights3)
+			weight=1/abs(v)
 			
-			else: # gerade
-				AddInterval(bin_width,initial,(N+1)*L,loc_prob3,weight,weights3)
-				AddInterval(bin_width,0,final,loc_prob3,weight,weights3)
-				for k in xrange(0,abs(b)-1): # ganze Strecken
-					AddInterval(bin_width,0,(N+1)*L,loc_prob3,weight,weights3)
+			if initial == 0:
+				initial=final
+				continue
+				
+			#---------------------------------------------------------------------------------
+			N=int(constants[0][2])
+			L=float(constants[4][2])
+			bin_width=(N+1)*L/bins
+			if b < 0:
+				if (b % 2) != 0: # ungerade
+					AddInterval(bin_width,0,initial,loc_prob2,weight,weights2)
+					AddInterval(bin_width,0,final,loc_prob2,weight,weights2)
+					for k in xrange(0,abs(b)-1): # ganze Strecken
+						AddInterval(bin_width,0,(N+1)*L,loc_prob2,weight,weights2)
 			
-		if b == 0:
-			if initial < final:
-				AddInterval(bin_width,initial,final,loc_prob1,weight,weights1)
-			else:
-				AddInterval(bin_width,final,initial,loc_prob1,weight,weights1)
-
-
+				else: # gerade
+					AddInterval(bin_width,0,initial,loc_prob2,weight,weights2)
+					AddInterval(bin_width,final,(N+1)*L,loc_prob2,weight,weights2)
+					for k in xrange(0,abs(b)-1): # ganze Strecken
+						AddInterval(bin_width,0,(N+1)*L,loc_prob2,weight,weights2)
+			
+	
+			if b > 0:
+				if (b % 2) != 0: # ungerade
+					AddInterval(bin_width,initial,(N+1)*L,loc_prob3,weight,weights3)
+					AddInterval(bin_width,final,(N+1)*L,loc_prob3,weight,weights3)
+					for k in xrange(0,abs(b)-1): # ganze Strecken
+						AddInterval(bin_width,0,(N+1)*L,loc_prob3,weight,weights3)
+			
+				else: # gerade
+					AddInterval(bin_width,initial,(N+1)*L,loc_prob3,weight,weights3)
+					AddInterval(bin_width,0,final,loc_prob3,weight,weights3)
+					for k in xrange(0,abs(b)-1): # ganze Strecken
+						AddInterval(bin_width,0,(N+1)*L,loc_prob3,weight,weights3)
+			
+			if b == 0:
+				if initial < final:
+					AddInterval(bin_width,initial,final,loc_prob1,weight,weights1)
+				else:
+					AddInterval(bin_width,final,initial,loc_prob1,weight,weights1)
+			#---------------------------------------------------------------------------------
+			
+			# Nicht vergessen:
+			initial=final
+	f.close()
+	
+constants=constants[:-2]
+print constants
 
 pp=PdfPages('output.pdf')		
 plt.figure()
 plt.hist([loc_prob1,loc_prob2,loc_prob3], bins=[q for q in arange(0,(N+1)*L + bin_width,bin_width)], normed=False, weights=[weights1,weights2,weights3], stacked=True)
 #plt.ylim(0.0, 0.7e+5)
 plt.savefig(pp,format='pdf')
-
 plt.clf()
-
-liste=particle_data[2*burn_in:2*burn_in+100]		
-t_axis=xrange(0, len(liste))
-x_axis=[q['pos'] for q in liste] 
-
-plt.plot(t_axis,x_axis)
-plt.savefig(pp, format='pdf')
-
-plt.clf()
-
-liste=particle_data[2*burn_in:3*burn_in]		
-t_axis=xrange(0, len(liste))
-x_axis=[q['pos'] for q in liste] 
-
-plt.plot(t_axis,x_axis)
-plt.savefig(pp, format='pdf')
-
-plt.clf()
-
-liste=particle_data[3*burn_in:4*burn_in]		
-t_axis=xrange(0, len(liste))
-x_axis=[q['pos'] for q in liste] 
-
-plt.plot(t_axis,x_axis)
-plt.savefig(pp, format='pdf')
-
 pp.close()
