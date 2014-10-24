39db0b596c77c32a851dcb419bc2f9d135901928
timlappe <lappet@student.ethz.ch>
2014-10-24 16:44:15 +0200

    friday

--- system.cc
+++ system.cc
@@ -1,315 +1,313 @@
-from numpy import *
-from math import *
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <omp.h>
+#include <vector>
+#include <math.h>
 
-matplotlib.rcParams["agg.path.chunksize"]=20000
+using namespace std;
 
-#---------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------
+// Systemkonstanten
+const unsigned int N=500;
 
-def AddInterval(bin_width,c,d,liste,weight,weights): # Interval [c,d]
-	for k in xrange(int(floor(c/bin_width)),int(ceil(d/bin_width))):
-		liste.append(k*bin_width + bin_width/2)
-		weights.append(weight)
-	return 0
+// Energielevel
+const unsigned int n=1;
 
-#---------------------------------------------------------------------------------
+// Boxlaenge
+const double a=1.0;
 
-constants=[]
-'''
-L=float(constants[4][2])
-N=int(constants[0][2])
-steps=int(constants[5][2])
-resol=int(constants[6][2])
-'''
-N=0
-L=0
-bin_width=0
+// Wirkungsquantum
+const double h=1.0;//6.62606957*1e-34;
 
-bins=50
+// Teilchenmasse
+const double M=1.0;//0.125;
 
-burn_in=100000
+// Energie
+const double E_0=n*n*h*h/(8*M*a*a);//0.125
 
-loc_prob1=[]
-weights1=[]
-loc_prob2=[]
-weights2=[]
-loc_prob3=[]
-weights3=[]
+// Gitterteilchenmasse
+const double m=10000*M;//100000000*M;
 
-particle_positions=[]
-particle_velocities=[]
-lattice_velocities_pre=[]
-#lattice_velocities_post=[]
-with open('particle_data.txt') as f:
-	counter=0
-	initial=0
-	for k, line in enumerate(f):
-		if line.startswith("#"):
-			strang=line.strip().split()
-			constants.append(strang)
-		else:	
-			string=line.strip().split()
-			particle_positions.append(float(string[2])) # 1 for real positions, 2 for indices
-			particle_velocities.append(float(string[3]))
-			lattice_velocities_pre.append(float(string[5]))
-			#lattice_velocities_post.append(float(string[5]))
+// Anzahl Zeitschritte
+const unsigned int steps=1000000;
+
+// Wiederkehrdauer
+const double delta_t=34.0;//506.0;// mind. t=16+2=18
+
+const double L=a/(N+1);
+const double k=(N+1)*(N+1)*m*E_0/(2*M*a*a);//(-1)*(1/((cos(n*M_PI/(N+1)) - 1)))*((m*h*h*pow(M_PI,2)*pow(n,4))/(32*M*M*pow(a,4)))
+
+// Matrizen
+vector<double>    Cos(N);
+vector<double>    Sin(N);
+vector<double> Sindot(N);
+
+vector< vector<double> >        T(N,vector<double>(N));
+vector< vector<double> >    T_Cos(N,vector<double>(N));
+vector< vector<double> >    T_Sin(N,vector<double>(N));
+vector< vector<double> > T_Sindot(N,vector<double>(N));
+
+vector< vector<double> >    R_Cos(N,vector<double>(N));
+vector< vector<double> >    R_Sin(N,vector<double>(N));
+vector< vector<double> > R_Sindot(N,vector<double>(N));
+
+// Diese Funktion laeuft nur einmal
+void TridiagToeplitz() {
+	cout << "------------------------------------------------\n";
+	cout << setprecision(10) << k << '\n';
+	cout << h/E_0 << '\n';
+	cout << 2*M_PI << "   " << sqrt(-((2*k/m)*(cos(n*M_PI/(N+1)) - 1)))/E_0 << "   " << (sqrt(-((2*k/m)*(cos(n*M_PI/(N+1)) - 1)))*delta_t)/(2*M_PI) << '\n';
+	for (int i=0; i<N; i++) {
+		//double delta_t=1.0;//h/E_0;
+		double EigVal=((2*k/m)*(cos((i+1)*M_PI/(N+1)) - 1));
+		//cout << i << "th " << "Eigenvalue: " << sqrt(-EigVal) << '\n';
+		Cos[i]=cos(sqrt(-EigVal)*delta_t);// ACHTUNG: assuming matrix D is neg. def.
+		   Sin[i]=sin(sqrt(-EigVal)*delta_t)/(sqrt(-EigVal)); // divided by omega
+		Sindot[i]=sin(sqrt(-EigVal)*delta_t)*(sqrt(-EigVal)); // multiplied by omega
 		
-			if k < (counter*int(constants[5][2]) + burn_in + 9):
-				continue
-				
-			if ((k-9) % int(constants[5][2])) == 0:
-				counter+=1
-				continue
-			#print k
-				
-			b=int(string[0])
-			final=float(string[1])
-			v=float(string[3])
+		// Wie kann ich diesen extra loop vermeiden?
+		double norm=0;
+		for (int j=0; j<N; j++) {
+			norm+=sin((j+1)*M_PI*(i+1)/(N+1))*sin((j+1)*M_PI*(i+1)/(N+1));
+		}
+		norm=sqrt(norm);
+		for (int j=0; j<N; j++) {
+			T[j][i]=sin((j+1)*M_PI*(i+1)/(N+1))/norm;
 			
-			weight=1/abs(v)
-			
-			if initial == 0:
-				initial=final
-				continue
-				
-			#---------------------------------------------------------------------------------
-			N=int(constants[0][2])
-			L=float(constants[4][2])
-			bin_width=(N+1)*L/bins
-			if b < 0:
-				if (b % 2) != 0: # ungerade
-					AddInterval(bin_width,0,initial,loc_prob2,weight,weights2)
-					AddInterval(bin_width,0,final,loc_prob2,weight,weights2)
-					for k in xrange(0,abs(b)-1): # ganze Strecken
-						AddInterval(bin_width,0,(N+1)*L,loc_prob2,weight,weights2)
-			
-				else: # gerade
-					AddInterval(bin_width,0,initial,loc_prob2,weight,weights2)
-					AddInterval(bin_width,final,(N+1)*L,loc_prob2,weight,weights2)
-					for k in xrange(0,abs(b)-1): # ganze Strecken
-						AddInterval(bin_width,0,(N+1)*L,loc_prob2,weight,weights2)
-			
+			// die Spalten der Matrix mit den Faktoren fuer die Zeitentwicklung ergaenzen (also sparsity ausnutzen)
+			T_Cos[j][i]=T[j][i]*Cos[i];
+			T_Sin[j][i]=T[j][i]*Sin[i];
+			T_Sindot[j][i]=T[j][i]*Sindot[i];
+		}
+	}
 	
-			if b > 0:
-				if (b % 2) != 0: # ungerade
-					AddInterval(bin_width,initial,(N+1)*L,loc_prob3,weight,weights3)
-					AddInterval(bin_width,final,(N+1)*L,loc_prob3,weight,weights3)
-					for k in xrange(0,abs(b)-1): # ganze Strecken
-						AddInterval(bin_width,0,(N+1)*L,loc_prob3,weight,weights3)
-			
-				else: # gerade
-					AddInterval(bin_width,initial,(N+1)*L,loc_prob3,weight,weights3)
-					AddInterval(bin_width,0,final,loc_prob3,weight,weights3)
-					for k in xrange(0,abs(b)-1): # ganze Strecken
-						AddInterval(bin_width,0,(N+1)*L,loc_prob3,weight,weights3)
-			
-			if b == 0:
-				if initial < final:
-					AddInterval(bin_width,initial,final,loc_prob1,weight,weights1)
-				else:
-					AddInterval(bin_width,final,initial,loc_prob1,weight,weights1)
-			#---------------------------------------------------------------------------------
-			
-			# Nicht vergessen:
-			initial=final
-	f.close()
+	// Matrizen berechnen, die Startwerte direkt mit Zeitentwicklung verbinden: z. B. T*Cos*T^-1
+	//parallel
+	#pragma omp parallel for
+	for (int i=0; i<N; i++) {
+		for (int j=0; j<N; j++) {
+			R_Cos[i][j]=0;
+			R_Sin[i][j]=0;
+			R_Sindot[i][j]=0;
+			for (int k=0; k<N; k++) {
+				   R_Cos[i][j]+=   T_Cos[i][k]*T[j][k]; // T transposed...
+				   R_Sin[i][j]+=   T_Sin[i][k]*T[j][k]; // T transposed...
+				R_Sindot[i][j]+=T_Sindot[i][k]*T[j][k]; // T transposed...
+			}
+		}
+	}
 	
-constants=constants[:-2]
-print constants
+}
 
-#---------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------
+class System {
+   public:
+   	// constructor
+	System(double, double, double*, double*);
+	
+	// Teilchen
+	double pos;
+	double v;
+	
+	// Gitter
+	double* x;
+	double* xdot;
+	
+	double* w;
+	double* wdot;
+	
+	// Methoden
+	double Collision(double, double, double, double);
+	void Oscillate();
+	double* Evolve(double*);
+};
 
-pp=PdfPages('output.pdf')		
+// initializing double delta_t, double pos, double v, double* x, double* xdot
+System::System(double pos_0, double v_0, double* x_0, double* xdot_0) {
+	pos=pos_0;
+	v=v_0;
+	
+	x=new double[N];
+	xdot=new double[N];
+	
+	w=new double[N];
+	wdot=new double[N];
 
-#---------------------------------------------------------------------------------
-# histogram
-fig1=plt.figure()
-plt.hist([loc_prob1,loc_prob2,loc_prob3], bins=[q for q in arange(0,(N+1)*L + bin_width,bin_width)], normed=False, weights=[weights1,weights2,weights3], stacked=True)
-plt.title('Location probability')
-plt.xlabel('pos')
-plt.ylabel('#')
-plt.grid(True)
-#plt.ylim(0.0, 0.7e+5)
-pp.savefig(fig1)
-#plt.clf()
+	x=x_0;
+	xdot=xdot_0;	
+}
 
-#---------------------------------------------------------------------------------
-time_plots=1
-time_start=0
-time_end=time_start + 500
-markersize=3
+double System::Collision(double m1, double v1, double m2, double v2) {
+	return 2*((m1*v1 + m2*v2)/(m1 + m2)) - v1;
+	// return 2*v2 - v1;
+}
 
-scatter_plots=0
-scatter_start=800000
-scatter_end=900000
-#---------------------------------------------------------------------------------
+void System::Oscillate() {
+	#pragma omp parallel for
+	for (int i=0; i<N; i++) {
+		w[i]=0.0;
+		wdot[i]=0.0;
+		for (int j=0; j<N; j++) {
+			w[i]+=(R_Cos[i][j]*x[j] + R_Sin[i][j]*xdot[j]);
+			wdot[i]+=(R_Cos[i][j]*xdot[j] - R_Sindot[i][j]*x[j]);
+		}
+	}
+	// interchange actual and previous values
+	double* help_ptr;
+	double* helpdot_ptr;
 
-if time_plots==1:
-	#---------------------------------------------------------------------------------
-	# positions
-	y_axis=particle_positions[time_start:time_end]	
-	t_axis=xrange(0, len(y_axis))
+	help_ptr=x;
+	helpdot_ptr=xdot;
 
-	fig2=plt.figure(figsize=(40,5))
-	plt.title('Trajectory')
-	plt.xlabel('t')
-	plt.ylabel('pos')
-	plt.plot(t_axis,y_axis,marker='o',markersize=markersize)
-	#plt.axis([time_start,time_end,0,1])
-	plt.grid(True)
-	pp.savefig(fig2)
+	x=w;
+	xdot=wdot;
+	
+	w=help_ptr;
+	wdot=helpdot_ptr;
+}
 
-	#---------------------------------------------------------------------------------
-		# positions
-	y_axis=particle_positions[time_end:2*time_end]	
-	t_axis=xrange(0, len(y_axis))
+double* System::Evolve(double* arr) {
 
-	fig2=plt.figure(figsize=(40,5))
-	plt.title('Trajectory')
-	plt.xlabel('t')
-	plt.ylabel('pos')
-	plt.plot(t_axis,y_axis,marker='o',markersize=markersize)
-	#plt.axis([time_start,time_end,0,1])
-	plt.grid(True)
-	pp.savefig(fig2)
+	// eine Gittermasse herausgreifen
+	int index=0;
+	index=round(pos/L) - 1;
+	
+	// Spezialfall Enden
+	if (index==N) { 
+		index-=1; // rechts
+	}
+	if (index==-1) {
+		index+=1; // links
+	}
+	
+	// neue Geschwindigkeiten berechnen
+	double w=v; // temporarily copy v
+	v=this->Collision(M, v, m, xdot[index]);
+	
+	double ww_pre=xdot[index]; // save xdot[index] before collision
+	xdot[index]=this->Collision(m, xdot[index], M, w); // use w
+	double ww_post=xdot[index]; // save xdot[index] after collision but before oscillation
 
-	#---------------------------------------------------------------------------------
-		# positions
-	y_axis=particle_positions[2*time_end:3*time_end]	
-	t_axis=xrange(0, len(y_axis))
+	// Position updaten
+	pos+=v*delta_t;//h/E_0;
+	
+	// Gitter weiterentwickeln
+	this->Oscillate(); // sets x and xdot to new values
+	
+	// Reflektion
+	double B=(N+1)*L; // B=a...
+	int b=floor(pos/B);
+	if ((b % 2) == 0) { // gerade
+		pos=pos - b*B;
+		// v bleibt
+	}
+	else { // ungerade
+		pos=B - (pos - b*B);
+		v=-v;
+	}
+	
+	// Position, resultierenden Index, (resultierende) Geschwindigkeiten des Teilchens und der Gittermasse speichern (v ist dann die Ausgangsgeschw. fuer den folgenden Stoss)
+	*arr=b;
+	arr++;
+	*arr=pos;
+	arr++;
+	*arr=index;
+	arr++;
+	*arr=v;
+	arr++;
+	*arr=ww_pre; // use ww_pre
+	arr++;
+	*arr=ww_post; // use ww_post
+	arr++;
+	
+	return arr;
+	
+	
+	// Dringend beachten: Das Teilchen fliegt mit der hier gespeicherten Geschw. v vom vorigen Ort zum hier gespeicherten Ort.
+	// Der Index gehoert zum vorigen Ort, weil er vor dem Update der Position gesetzt wird.
+	// b gehoert hingegen zur aktuellen, weil upgedateten Position.
+	
+}
 
-	fig2=plt.figure(figsize=(40,5))
-	plt.title('Trajectory')
-	plt.xlabel('t')
-	plt.ylabel('pos')
-	plt.plot(t_axis,y_axis,marker='o',markersize=markersize)
-	#plt.axis([time_start,time_end,0,1])
-	plt.grid(True)
-	pp.savefig(fig2)
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
-	#---------------------------------------------------------------------------------
-	# velocities
-	y_axis=particle_velocities[time_start:time_end]
-	t_axis=xrange(0, len(y_axis))
+int main() {
 
-	fig3=plt.figure(figsize=(40,5))
-	plt.title('Velocity (post)')
-	plt.xlabel('t')
-	plt.ylabel('v')
-	plt.plot(t_axis,y_axis)
-	plt.grid(True)
-	pp.savefig(fig3)
+// Matrizen erzeugen
+TridiagToeplitz();
 
-	#---------------------------------------------------------------------------------
-		# velocities
-	y_axis=particle_velocities[time_end:2*time_end]
-	t_axis=xrange(0, len(y_axis))
+// Raeumliche Aufloesung Anfangswerte
+const unsigned int resol=1;
+double pos_0s[resol]={36.0};
 
-	fig3=plt.figure(figsize=(40,5))
-	plt.title('Velocity (post)')
-	plt.xlabel('t')
-	plt.ylabel('v')
-	plt.plot(t_axis,y_axis)
-	plt.grid(True)
-	pp.savefig(fig3)
+vector<double> particle_data_array(6*resol*steps, 0.0); // 6=number of elements in each file line
 
-	#---------------------------------------------------------------------------------
-		# velocities
-	y_axis=particle_velocities[2*time_end:3*time_end]
-	t_axis=xrange(0, len(y_axis))
+// Anfangswerte Gitter
+double x_0[N]={};
+double xdot_0[N]={};
+double y_0[N]={};
+double ydot_0[N]={};
 
-	fig3=plt.figure(figsize=(40,5))
-	plt.title('Velocity (post)')
-	plt.xlabel('t')
-	plt.ylabel('v')
-	plt.plot(t_axis,y_axis)
-	plt.grid(True)
-	pp.savefig(fig3)
+ydot_0[n-1]=0.005;//15.0;//0.1118;//0.005;
 
-	#---------------------------------------------------------------------------------
-	# lattice velocities pre and post
-	y_axis_1=lattice_velocities_pre[time_start:time_end]	
-	#y_axis_2=lattice_velocities_post
-	t_axis=xrange(0, len(y_axis_1))
+	for (int i=0; i<N; i++) {
+		for (int j=0; j<N; j++) {
+			x_0[i]+=T[i][j]*y_0[j];
+			xdot_0[i]+=T[i][j]*ydot_0[j];
+			
+		}
+		//cout << xdot_0[i] << "\n";
+	}
 
-	fig3=plt.figure(figsize=(40,5))
-	plt.title('Current lattice velocity')
-	plt.xlabel('t')
-	plt.ylabel('xdot')
-	plt.plot(t_axis,y_axis_1)
-	#plt.plot(t_axis,y_axis_2)
-	plt.grid(True)
-	pp.savefig(fig3)
+double energie=0;
+	for (int i=0; i<N; i++) {
+			energie+=0.5*m*xdot_0[i]*xdot_0[i];
+	}
+	
+cout << "Energie: " << energie <<", E_0: " << E_0 << '\n';
+cout << "\n";
 
-	#---------------------------------------------------------------------------------
+// Anfangsgeschwindigkeit Teilchen
+double v_0=0.0;//sqrt(2*E_0/M);
 
-#---------------------------------------------------------------------------------
+double* ptr;
+ptr=&particle_data_array[0];
 
-if scatter_plots==1:
-	#---------------------------------------------------------------------------------
-	# recursion plot positions
-	liste1=particle_positions[:-1]
-	liste2=particle_positions[1:]
-	liste1=liste1[scatter_start:scatter_end]
-	liste2=liste2[scatter_start:scatter_end]
+for (int ii=0; ii<resol; ii++) {
+	// Anfangsposition Teilchen
+	double pos_0=pos_0s[ii]*L;
 
-	fig4=plt.figure()
-	plt.scatter(liste1,liste2,s=0.1)
-	plt.title('Recursion plot - positions of collisions')
-	plt.xlabel('pos_i')
-	plt.ylabel('pos_i+1')
-	plt.axis([0,1,0,1])
-	plt.grid(True)
-	pp.savefig(fig4)
-	#---------------------------------------------------------------------------------
-	# recursion plot velocities
-	liste1=particle_velocities[:-1]
-	liste2=particle_velocities[1:]
-	liste1=liste1[scatter_start:scatter_end]
-	liste2=liste2[scatter_start:scatter_end]
+	System sys(pos_0, v_0, x_0, xdot_0);
 
-	fig5=plt.figure()
-	plt.title('Recursion plot - velocities (post)')
-	plt.xlabel('v_i')
-	plt.ylabel('v_i+1')
-	plt.scatter(liste1,liste2,s=0.1)
-	plt.grid(True)
-	pp.savefig(fig5)
-	#---------------------------------------------------------------------------------
-	# recursion plot lattice velocities
-	liste1=lattice_velocities_pre[:-1]
-	liste2=lattice_velocities_pre[1:]
-	liste1=liste1[scatter_start:scatter_end]
-	liste2=liste2[scatter_start:scatter_end]
+	for (int i=0; i<steps; i++) {
+		ptr=sys.Evolve(ptr);
+	}
+}
 
-	fig6=plt.figure()
-	plt.scatter(liste1,liste2,s=0.1)
-	plt.title('Recursion plot - velocity of current lattice point (post)')
-	plt.xlabel('xdot_i')
-	plt.ylabel('xdot_i+1')
-	plt.grid(True)
-	pp.savefig(fig6)
-	#---------------------------------------------------------------------------------
-	# scatter plot mixed velocities
-	liste1=particle_velocities
-	liste2=lattice_velocities_pre
-	liste1=liste1[scatter_start:scatter_end]
-	liste2=liste2[scatter_start:scatter_end]
+// Textdatei anlegen und oeffnen
+ofstream particle_data;
+particle_data.open("particle_data.txt");
 
-	fig6=plt.figure()
-	plt.scatter(liste1,liste2,s=0.1)
-	plt.title('Scatter plot - velocity of particle (post) and current lattice mass')
-	plt.xlabel('v_i')
-	plt.ylabel('xdot_i')
-	plt.grid(True)
-	pp.savefig(fig6)
-#---------------------------------------------------------------------------------
+particle_data << "# N: " << N << '\n';
+particle_data << "# M: " << M << '\n';
+particle_data << "# m: " << m << '\n';
+particle_data << "# v_0: " << v_0 << '\n';
+particle_data << "# L: " << L << '\n'; 
+particle_data << "# steps: " << steps << '\n';  
+particle_data << "# resol: " << resol << '\n'; 
+particle_data << "# ----------------------------" << '\n';
+particle_data << "# ----------------------------" << '\n';
 
-pp.close()
+// array einlesen
+for (int i=0; i<resol*steps; i++) {
+	for (int j=i*6; j<(i+1)*6; j++) { // 6=number of elements in each file line
+		particle_data << particle_data_array[j] << '\t'; // possibly use << setw(15) or so
+	}
+	particle_data << '\n';
+}
 
-#---------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------
+particle_data.close();
+
+return 0;
+}
